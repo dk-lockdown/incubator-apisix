4d1bbdf2cfeeee4ad26335169e7d3e052ef94367
liuxiaomin <dugukuangshao@qq.com>
2020-05-09 14:09:08 +0800

    token-extract addon

--- apisix/plugins/token-extract.lua
+++ apisix/plugins/token-extract.lua
@@ -1,4 +1,3 @@
-
 local core     = require("apisix.core")
 local json     = require("apisix.core.json")
 local http     = require("resty.http")
@@ -6,6 +5,7 @@ local ngx = ngx
 
 local plugin_name = "token-extract"
 
+-- 此插件配合 sso，通过 authrization 提取用户的 userId、userName 放到 requestHeader
 local schema = {
     type = "object",
     properties = {
@@ -28,6 +28,13 @@ function _M.check_schema(conf)
     end
 
     return true
+end
+
+local function fail_response(code, message, init_values)
+    local response = init_values or {}
+    response.code = code
+    response.msg = message
+    return response
 end
 
 local function new_headers()
@@ -75,10 +82,33 @@ function _M.rewrite(conf, ctx)
     local token = core.request.header(ctx, "Authorization")
     if token then
         local headers = new_headers()
+        headers["Content-Type"] = "application/json; charset=utf-8"
         headers["Authorization"] = token
         local timeout = 1000 * 10
 
-        res, err = http_get(conf.tokenExtractUrl, headers, timeout)
-        
+        local res, err = http_get(conf.tokenExtractUrl, headers, timeout)
+        if err or not res then
+            return 500, fail_response(500,"校验用户token失败")
+        else
+            local body, err = json.decode(res.body)
+            if err or not body.success then
+                return 500, fail_response(500,"校验用户token失败")
+            end
+            if body.data.code == 1 then
+                ngx.req.set_header("uid", body.data.userId)
+                ngx.req.set_header("un", body.data.userName)
+            end
+            if body.data.code == 0 then
+                return 401, fail_response(401,"您的用户凭证已过期，请重新登录")
+            end
+            if body.data.code == -1 then
+                return 500, fail_response(500,"校验用户token失败")
+            end
+            if body.data.code == -2 then
+                return 401, fail_response(401,"用户已其他地方登录，若非本人操作，建议您修改密码")
+            end
+        end
     end
 end
+
+return _M
